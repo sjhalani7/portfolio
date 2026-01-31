143ace5d8fe1b0122f48fd70a0542e4b9d4d28e3
sjhalani7 <86908164+sjhalani7@users.noreply.github.com>
2026-01-31 20:55:26 +0530

    update portfolio env to fetch right settings

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,6 @@
+const repoName = "portfolio";
+const isProd = process.env.NODE_ENV === "production";
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: "export",
@@ -5,6 +8,8 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: isProd ? `/${repoName}` : undefined,
+  assetPrefix: isProd ? `/${repoName}/` : undefined,
 };
 
 export default nextConfig;
